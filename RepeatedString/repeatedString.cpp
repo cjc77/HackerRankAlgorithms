0f70d8566f932ef800225f8a4a3e6968712566e6
carson <carsonjamescook@gmail.com>
2016-09-14 18:06:31 -0700

    New Stuff.

---
+++ RepeatedString/repeatedString.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int findA(string, int);
+
+int findA(string s, int n) {
+    int count = 0; 
+    return count;
+}
+
+int main()
+{
+    string s = " ";
+    int n = 0;
+    cin >> s;
+    cin >> n;
+    int occ = findA(s, n);
+    cout << occ << '\n';
+    return 0;
+}
